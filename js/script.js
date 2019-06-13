668ea103601bdd1a101be3d4b198463e6ac52d6a
nemanja13 <nemanja.maksimovic.4.18@ict.edu.rs>
2019-06-13 16:36:01 +0200

    kreiran sajt

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".dropdown-trigger").dropdown()
+});
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+$(document).ready(function(){
+    $('.slider').slider();
+});
+$(document).ready(function(){
+    $('.modal').modal();
+});
