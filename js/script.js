340bb934de73f001686bdaf4a1adb5136d070330
nemanja13 <nemanja.maksimovic.4.18@ict.edu.rs>
2019-06-22 14:33:57 +0200

    Proizvodi asus

--- js/script.js
+++ js/script.js
@@ -10,3 +10,21 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('.modal').modal();
 });
+var limitSlider = document.getElementById('slider-limit');
+
+noUiSlider.create(limitSlider, {
+    start: [25000, 200000],
+    limit: 200000,
+    behaviour: 'drag',
+    connect: true,
+    range: {
+        'min': 25000,
+        'max': 200000
+    }
+});
+var limitFieldMin = document.getElementById('slider-limit-value-min');
+var limitFieldMax = document.getElementById('slider-limit-value-max');
+
+limitSlider.noUiSlider.on('update', function (values, handle) {
+    (handle ? limitFieldMax : limitFieldMin).innerHTML = values[handle];
+});
